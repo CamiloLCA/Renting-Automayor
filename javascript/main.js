ac2d8cd7f6c02debabf50d5f8589e589fc46a3ca
CamiloLCA <camian06@hotmail.com>
2022-04-26 10:34:23 -0500

    Nueva ventana de amortizacion y pequeños cambios

--- javascript/main.js
+++ javascript/main.js
@@ -18,6 +18,9 @@ listElements.forEach(listElement => {
     })
 });
 
+menuToggle.addEventListener('click', ()=>{
+    listResponsive.classList.toggle('nav--translate');
+});
 
 let stepElementsButton = document.querySelectorAll('.steps__list-button--second-step');
 
@@ -60,6 +63,44 @@ thirdStepElementsButton.forEach(thirdStepElementButton =>{
     })
 })
 
-menuToggle.addEventListener('click', ()=>{
-    listResponsive.classList.toggle('nav--translate');
-});
+let fivethStepElementsButton = document.querySelectorAll('.steps__list-button--fiveth-step');
+
+fivethStepElementsButton.forEach(fivethStepElementButton =>{
+    fivethStepElementButton.addEventListener('click', ()=>{
+
+        let fivethStepElementMenu = document.querySelector('.list__show--third-menu');
+        fivethStepElementButton.nextElementSibling.classList.toggle('steps__list-info--visible');
+
+        let infoHeight = 0;
+        let showInfo = fivethStepElementButton.nextElementSibling;
+        if(showInfo.clientHeight >= "0"){
+            infoHeight = showInfo.scrollHeight;
+        }
+
+        fivethMenuHeight = fivethStepElementMenu.scrollHeight;
+
+        fivethStepElementMenu.style.height = `${fivethMenuHeight}px`;
+        showInfo.style.height = `${infoHeight}px`;
+    })
+})
+
+let sixthStepElementsButton = document.querySelectorAll('.steps__list-button--sixth-step');
+
+sixthStepElementsButton.forEach(sixthStepElementButton =>{
+    sixthStepElementButton.addEventListener('click', ()=>{
+
+        let sixthStepElementMenu = document.querySelector('.list__show--fourth-menu');
+        sixthStepElementButton.nextElementSibling.classList.toggle('steps__list-info--visible');
+
+        let infoHeight = 0;
+        let showInfo = sixthStepElementButton.nextElementSibling;
+        if(showInfo.clientHeight >= "0"){
+            infoHeight = showInfo.scrollHeight;
+        }
+
+        sixthMenuHeight = sixthStepElementMenu.scrollHeight;
+
+        sixthStepElementMenu.style.height = `${sixthMenuHeight}px`;
+        showInfo.style.height = `${infoHeight}px`;
+    })
+})
